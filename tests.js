3761da254d4a9693a0932d66c93d3b5d007c29e4
Hugo Des <hugo.deslongchamps@gmail.com>
2014-04-28 19:01:52 +0200

    les tests marchent !!

--- tests.js
+++ tests.js
@@ -1,3 +1,12 @@
 var assert = require('chai').assert;
  
-var Hotel = require('./models').HousingSchema;
+var City = require('./models').City;
+
+describe("City", function  () {
+	describe("constructor", function  () {
+		it("should have a name", function  () {
+			var hebergement = new City( { name : 'Valence' } );
+			assert.equal(hebergement.name, 'Valence', 'Le nom de vile est bien "Valence" ')
+		})
+	})
+})
